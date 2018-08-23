211b85f42a032a1b6fa4d8513ab14d50ce1eedd6
Xingjian Long <tdlxj@tju.edu.cn>
2018-08-23 20:59:17 +0800

    第一题

---
+++ src/Solution1.java
@@ -0,0 +1,16 @@
+class Solution1 {
+    public int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+        outer:
+        for(int i=0;i<=nums.length-2;i++){
+            for(int j=i+1;j<=nums.length-1;j++){
+                if(nums[i]+nums[j] == target){
+                    result[0] = i;
+                    result[1] = j;
+                    break outer;
+                }
+            }
+        }
+        return result;
+    }
+}
