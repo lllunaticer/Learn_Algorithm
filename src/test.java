ea465864290ee9e03d27c8c150b45d33ce8c68e0
Xingjian Long <tdlxj@tju.edu.cn>
2018-11-06 18:56:39 +0800

    第12题，分治思想

--- src/test.java
+++ src/test.java
@@ -1,9 +1,10 @@
 public class test {
     public static void main(String[] args){
-        int i;
-        for(i=0; i<=10;i++){
-            //System.out.println(i);
-        }
+        int n = 1134;
+        int i = (n-n%1000)/1000;
+//        for(i=0; i<=10;i++){
+//            //System.out.println(i);
+//        }
         System.out.println(i);
     }
 }
