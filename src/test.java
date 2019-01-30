c5a31b37b631d1eb01de8e10de06e5e603443ca3
Xingjian Long <tdlxj@tju.edu.cn>
2019-01-30 21:37:53 +0800

    4道简单链表

--- src/test.java
+++ src/test.java
@@ -1,14 +1,6 @@
-
 public class test {
     public static void main(String[] args){
-//        System.out.println(0xff>>>7);
-//        System.out.println(0xff);
-//        System.out.println(((byte)0xff)>>>7);
-//        System.out.println((byte)(((byte)0xff)>>>7));
-//
-//        System.out.println(1>>3);
-//        System.out.println(1L>>3);
-        System.out.println(Byte.MAX_VALUE);
-        System.out.println(Byte.MIN_VALUE);
+
+        System.out.println(2>>1);
     }
 }
